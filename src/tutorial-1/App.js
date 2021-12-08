5299c0b8411c7a1ceed2fcf3adcf772757fc7fc6
HauptmannWilbert <theredkss@gmail.com>
2021-12-08 23:08:00 +0300

    Тестовая отправка

---
+++ src/tutorial-1/App.js
@@ -0,0 +1,13 @@
+
+function App(){
+    return(
+        <div className="lessons">
+            <div>
+                <h1>Урок 1</h1>
+                <p>Тест</p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
